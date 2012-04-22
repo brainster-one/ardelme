b5658fe6792cc49b0837a6d56018598ee8439a4c
brainster-one <brainster-one@hotmail.com>
2012-04-22 16:14:02 +0700

    Build pipeline.

---
+++ build.cmd
@@ -0,0 +1,4 @@
+Assets\MSBuild.exe .\Sources\Ardelme.proj
+Assets\MSBuild.exe .\Sources\Ardelme.Silverlight.proj
+
+Assets\NuGet.exe pack .\Packages\Specifications\Ardelme.nuspec -OutputDirectory packages
